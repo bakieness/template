eb19ca87995f0b464370b114b1f1b5123f1e0a13
James Baker <jamesbaker_1993@hotmail.co.uk>
2016-12-14 17:11:43 +0000

    removed comments from gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,11 @@ var browserSync = require('browser-sync').create();
 gulp.task('watch', ['browserSync', 'sass'], function(){
   gulp.watch(['www/SCSS/site.scss', 'www/SCSS/_config.scss', 'www/SCSS/_mixins.scss', 'www/SCSS/_reset.scss'], ['sass']); 
   gulp.watch('www/*.html', browserSync.reload); 
-  // Other watchers
 })
 
 gulp.task('sass', function(){
   return gulp.src('www/SCSS/site.scss')
-    .pipe(sass()) // Using gulp-sass
+    .pipe(sass())
     .pipe(gulp.dest('www/CSS'))
     .pipe(browserSync.reload({
       stream: true
