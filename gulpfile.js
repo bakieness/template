8b47e25cbde0567655413d9ab201a5b2b93ccaca
bakieness <jamesbaker_1993@hotmail.co.uk>
2016-12-14 16:59:55 +0000

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
 gulp.task('watch', ['browserSync', 'sass'], function(){
-  gulp.watch(['www/SCSS/site.scss' 'www/SCSS/_config.scss' 'www/SCSS/_mixins.scss' 'www/SCSS/_reset.scss'], ['sass']); 
+  gulp.watch(['www/SCSS/site.scss', 'www/SCSS/_config.scss', 'www/SCSS/_mixins.scss', 'www/SCSS/_reset.scss'], ['sass']); 
   gulp.watch('www/*.html', browserSync.reload); 
   // Other watchers
 })
