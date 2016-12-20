5eb34f370a6b8e2fddff53086c0a7c8ddd8b993f
James Baker <jamesbaker_1993@hotmail.co.uk>
2016-12-20 17:18:05 +0000

    new sass

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
 var del = require('del');
 var runSequence = require('run-sequence');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
 
 gulp.task('watch', ['browserSync', 'sass'], function(){
   gulp.watch('www/SCSS/*.scss', ['sass']); 
@@ -15,7 +17,7 @@ gulp.task('watch', ['browserSync', 'sass'], function(){
 })
 
 gulp.task('sass', function(){
-  return gulp.src('www/SCSS/site.scss')
+  return gulp.src('www/SCSS/style.scss')
     .pipe(sass())
     .pipe(gulp.dest('www/CSS'))
     .pipe(browserSync.reload({
@@ -40,15 +42,29 @@ gulp.task('useref', function(){
     .pipe(gulp.dest('dist'))
 })
 
+gulp.task('php', function(){
+  return gulp.src('www/*.php')
+    .pipe(gulp.dest('dist'))
+})
+
 gulp.task('clean:dist', function() {
   return del.sync('dist');
 })
+
+gulp.task('images', function(){
+  return gulp.src('www/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('dist/images'))
+});
 
 gulp.task('default', ['watch'])
 
 gulp.task('build', function (callback) {
   runSequence('clean:dist', 
-    ['sass', 'useref'],
+    ['sass', 'useref', 'php', 'images'],
     callback
   )
 });
